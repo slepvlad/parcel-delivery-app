10d2be2083b2cffc82af0a8b7d0d04e692f6ad6b
Slepov Vladimir <slepovva@gmail.com>
2023-03-08 13:27:16 +0700

    create api gateway and service discovery

--- users/src/main/java/com/guavapay/parceldeliveryapp/UsersApplication.java
+++ users/src/main/java/com/guavapay/parceldeliveryapp/UsersApplication.java
@@ -4,9 +4,11 @@ import com.guavapay.parceldeliveryapp.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
+@EnableDiscoveryClient
 public class UsersApplication {
 
 	public static void main(String[] args) {
